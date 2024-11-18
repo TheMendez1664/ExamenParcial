34d5e5b27cc91d13396d8de835cf4c8b456cc843
KevinRuiz <kevin_ruiz_sanchez@hotmail.com>
2024-11-18 08:09:35 -0500

    Proyecto Base

---
+++ Kevin-Ruiz-Sanchez/src/main/java/ExamenParcial/Kevin/Ruiz/Sanchez/KevinRuizSanchezApplication.java
@@ -0,0 +1,13 @@
+package ExamenParcial.Kevin.Ruiz.Sanchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinRuizSanchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinRuizSanchezApplication.class, args);
+	}
+
+}
---
+++ Kevin-Ruiz-Sanchez/src/test/java/ExamenParcial/Kevin/Ruiz/Sanchez/KevinRuizSanchezApplicationTests.java
@@ -0,0 +1,13 @@
+package ExamenParcial.Kevin.Ruiz.Sanchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinRuizSanchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
