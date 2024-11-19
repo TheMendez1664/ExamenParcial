c4a90e1931d19457c4a3b4b73f89a30e57e7d0b9
KevinRuiz <kevin_ruiz_sanchez@hotmail.com>
2024-11-19 09:19:38 -0500

    Centralización de rutas

---
+++ Kevin-Ruiz-Sanchez/src/main/java/ExamenParcial/Kevin/Ruiz/Sanchez/config/ApiRoutes.java
@@ -0,0 +1,10 @@
+package ExamenParcial.Kevin.Ruiz.Sanchez.config;
+
+public class ApiRoutes {
+    public static final String API_ALUMNO = "/api/alumno";
+    public static final String FIND_ALL = "/findAll";
+    public static final String GET_BY_ID = "/getById";
+    public static final String CREATE = "/create";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete";
+}
